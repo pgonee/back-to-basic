a7dc3d4395e6b1eb4525cfc779dc85bac13ca682
pgonee <pgonee@gmail.com>
2025-04-19 21:23:17 +0900

    solve https://leetcode.com/problems/two-sum/

--- leetcode/1_two_sum/main.cpp
+++ leetcode/1_two_sum/main.cpp
@@ -1,6 +1,39 @@
+#include <cassert>
 #include <iostream>
+#include <vector>
 
-int main(int argc, char *argv[]) {
-  std::cout << "Hello?" << std::endl;
-  return 0;
+using namespace std;
+
+class Solution {
+public:
+  Solution() {}
+
+  vector<int> twoSum(vector<int> &nums, int target) {
+    int startIndex = 0;
+    vector<int> v;
+
+    while (startIndex < nums.size() - 1) {
+      int a = nums[startIndex];
+      for (size_t i = startIndex + 1; i < nums.size(); i++) {
+        int b = nums[i];
+        if ((a + b) == target) {
+          v.push_back(startIndex);
+          v.push_back(i);
+          goto jump;
+        }
+      }
+
+      startIndex += 1;
+    }
+
+  jump:
+    return v;
+  }
+};
+
+int main() {
+  Solution s;
+  vector<int> a = {3, 2, 3};
+
+  assert((s.twoSum(a, 6) == vector<int>{0, 2}));
 }
